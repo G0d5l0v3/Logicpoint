e7329fc155500d9c533a29f42d4fc48536c7453f
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-09-09 00:45:14 +0100

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
             <p className="text-white font-[poppins-regular] pt-[0.5rem] text-[0.6rem] md:text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing </p>
             <p className="text-white font-[poppins-regular] text-[0.6rem] md:text-sm">elit. Ut elit tellus, luctus nec ullamcorper mattis,</p>
             <p className="text-white font-[poppins-regular] text-[0.6rem] md:text-sm">pulvinar dapibus leo.</p>
-            <a href="" alt="learnMore">
+            <a href="https://google.com" alt="learnMore">
               <div className="px-[1rem] py-[0.5rem] mt-[1rem] text-[#E87B37] font-[poppins-regular] text-[0.7rem] sm:text-[0.6rem] md:text-sm border-[#E87B37] text-align text-center border-2 w-[7rem] sm:w-[8rem] md:w-[10rem]">Learn More</div>
             </a>
           </div>
