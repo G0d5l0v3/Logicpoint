cbff1e51da40ffb334b1e152ee6c647608e5926c
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-09-09 09:53:07 +0100

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,41 @@
 import React from "react";
+import { useState } from "react"
 import logo from "../../Assets/Images/Logicpoint-Logo.svg";
 import { NavLink } from "react-router-dom";
 import "../../index.css";
 
 const Home = () => {
+
   return (
     <React.Fragment>
       <header className="min-h-screen w-full bg px-[10%]">
         <nav className="flex items-center justify-between text-white text-sm font-[poppins-regular] pt-[0.5rem]">
           <img src={logo} alt="logo" />
-          <NavLink className=" hover:text-[#E87B37] focus:text-[#E87B37] text-white transition duration-500 ease-in-out">HOME</NavLink>
-          <NavLink className=" hover:text-[#E87B37] focus:text-[#E87B37] text-white transition duration-500 ease-in-out">ABOUT US</NavLink>
-          <NavLink className=" hover:text-[#E87B37] focus:text-[#E87B37] text-white transition duration-500 ease-in-out">SERVICES</NavLink>
-          <NavLink className=" hover:text-[#E87B37] focus:text-[#E87B37] text-white transition duration-500 ease-in-out">PARTNERS</NavLink>
+          <NavLink
+            className= "transition duration-300 ease-in-out hover:border-b-2 hover:border-[#E87B37] hover:text-[#E87B37] focus:text-[#E87B37] focus:border-b-2 focus:border-[#E87B37]"
+            to=""
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            className= "transition duration-300 ease-in-out hover:border-b-2 hover:border-[#E87B37] hover:text-[#E87B37] focus:text-[#E87B37] focus:border-b-2 focus:border-[#E87B37]"
+            to=""
+
+          >
+            ABOUT US
+          </NavLink>
+          <NavLink
+            className= "transition duration-300 ease-in-out hover:border-b-2 hover:border-[#E87B37] hover:text-[#E87B37] focus:text-[#E87B37] focus:border-b-2 focus:border-[#E87B37]"
+            to=""
+          >
+            SERVICES
+          </NavLink>
+          <NavLink
+            className= "transition duration-300 ease-in-out hover:border-b-2 hover:border-[#E87B37] hover:text-[#E87B37] focus:text-[#E87B37] focus:border-b-2 focus:border-[#E87B37]"
+            to=""
+          >
+            PARTNERS
+          </NavLink>
           <button className="bg-[#E87B37] px-[1rem] py-[0.5rem] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
             CONTACT US
           </button>
@@ -37,7 +60,7 @@ const Home = () => {
               pulvinar dapibus leo.
             </p>
 
-            <button className="px-[1rem] py-[0.5rem] mt-[1rem] text-[#E87B37] font-[poppins-regular] text-[0.7rem] sm:text-[0.6rem] md:text-sm lg:text-lg border-[#E87B37] text-align text-center border-2 w-[7rem] sm:w-[8rem] md:w-[10rem]">
+            <button className="px-[1rem] py-[0.5rem] mt-[1rem] text-[#E87B37] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-[poppins-regular] text-[0.7rem] sm:text-[0.6rem] md:text-sm lg:text-lg border-[#E87B37] text-align text-center border-2 w-[7rem] sm:w-[8rem] md:w-[10rem]">
               Learn More
             </button>
           </div>
