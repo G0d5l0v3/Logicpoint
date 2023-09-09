c2807af72363e959070c81c4f1df20a92b3d2566
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-09-09 17:51:18 +0100

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import { useState } from "react";
 import logo from "../../Assets/Images/Logicpoint-Logo.svg";
 import { NavLink } from "react-router-dom";
-import { Modal } from "antd";
+import { motion } from "framer-motion";
 import "../../index.css";
 
 const Home = () => {
+  const [isAnimating, setIsAnimating] = useState(false);
+  const handleButtonClick = () => {
+    setIsAnimating(!isAnimating);
+  };
+
   const [clicked, setClicked] = useState(false);
   const handleClick = () => {
     setClicked(!clicked);
@@ -19,7 +24,7 @@ const Home = () => {
             <img src={logo} alt="logo" className="w-full" />
           </div>
 
-          <div className= "hidden md:flex md:flex-row md:items-center md:justify-between md:w-full">
+          <div className="hidden md:flex md:flex-row md:items-center md:justify-between md:w-full">
             <NavLink
               className="text-[0.6rem] md:text-[0.85rem] transition duration-300 ease-in-out hover:border-b-2 hover:border-[#E87B37] hover:text-[#E87B37] focus:text-[#E87B37] focus:border-b-2 focus:border-[#E87B37]"
               to=""
@@ -49,7 +54,16 @@ const Home = () => {
             </button>
           </div>
 
-          <div className={ clicked ? "flex flex-col fixed right-0 top-[5rem] p-[3rem]  bg-[#5D1570] w-[30%] justify-start items-start md:hidden": "hidden"}>
+          <div
+            className={
+              clicked
+                ? "flex flex-col fixed right-0 top-[5rem] p-[3rem]  bg-[#5D1570] w-[30%] transition duration-300 ease-in-out justify-start items-start md:hidden"
+                : "hidden"
+            }
+            initial={{ opacity: 0.4, x: 0 }}
+            animate={{ opacity: 1, x: 20 }}
+            transition={{ ease: "easeInOut", duration: 5 }}
+          >
             <NavLink
               className="text-[0.5rem]  sm:text-[0.6rem] md:text-[0.85rem] transition duration-300 ease-in-out hover:border-b-2 hover:border-[#E87B37] hover:text-[#E87B37] focus:text-[#E87B37] focus:border-b-2 focus:border-[#E87B37]"
               to=""
@@ -78,7 +92,6 @@ const Home = () => {
               CONTACT US
             </button>
           </div>
-          
 
           <div className="md:hidden">
             <i
@@ -90,7 +103,6 @@ const Home = () => {
               onClick={handleClick}
             ></i>
           </div>
-
         </nav>
 
         <main className="mt-[10rem]">
