a6d8a4837493ef728c3d3e3010a07f2432781f11
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-09-10 09:36:30 +0100

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,13 +7,11 @@ import "../../index.css";
 
 const Home = () => {
   const [isAnimating, setIsAnimating] = useState(false);
-  const handleAnimation = () => {
-    setIsAnimating(!isAnimating);
-  };
-
   const [clicked, setClicked] = useState(false);
+
   const handleClick = () => {
     setClicked(!clicked);
+    setIsAnimating(!isAnimating);
   };
 
   return (
@@ -32,7 +30,7 @@ const Home = () => {
               HOME
             </NavLink>
             <NavLink
-              className="text-[0.6rem] md:text-[0.85rem] transition duration-300 ease-in-out hover:border-b-2 hover:border-[#E87B37] hover:text-[#E87B37] focus:text-[#E87B37] focus:border-b-2 focus:border-[#E87B37]"
+              className="text-[0.6rem] md:text-[0.85rem] md:text-center transition duration-300 ease-in-out hover:border-b-2 hover:border-[#E87B37] hover:text-[#E87B37] focus:text-[#E87B37] focus:border-b-2 focus:border-[#E87B37]"
               to=""
             >
               ABOUT US
@@ -49,20 +47,22 @@ const Home = () => {
             >
               PARTNERS
             </NavLink>
-            <button className="text-[0.5rem]  sm:text-[0.6rem]  mt-[1rem] md:text-[0.85rem] bg-[#E87B37] px-[1rem] py-[0.5rem] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+            <button className="text-[0.5rem]  sm:text-[0.6rem] md:text-[0.85rem] bg-[#E87B37] px-[1rem] py-[0.5rem] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
               CONTACT US
             </button>
           </div>
 
-          <div
+          <motion.div
             className={
               clicked
-                ? "flex flex-col fixed right-0 top-[5rem] p-[3rem]  bg-[#5D1570] w-[30%] transition duration-300 ease-in-out justify-start items-start md:hidden"
+                ? "flex flex-col fixed right-0 top-[5rem] p-[3rem]  bg-[#000] w-[30%] transition duration-300 ease-in-out justify-start items-start md:hidden"
                 : "hidden"
             }
-            initial={{ opacity: 0.4, x: 0 }}
-            animate={{ opacity: 1, x: 20 }}
-            transition={{ ease: "easeInOut", duration: 5 }}
+            initial={false}
+            animate={
+              isAnimating ? { opacity: 1, x: 0 } : { opacity: 0, x: 100 }
+            }
+            transition={{ ease: "easeInOut", duration: 0.3 }}
           >
             <NavLink
               className="text-[0.5rem]  sm:text-[0.6rem] md:text-[0.85rem] transition duration-300 ease-in-out hover:border-b-2 hover:border-[#E87B37] hover:text-[#E87B37] focus:text-[#E87B37] focus:border-b-2 focus:border-[#E87B37]"
@@ -91,7 +91,7 @@ const Home = () => {
             <button className="text-[0.4rem]  sm:text-[0.6rem] mt-[1rem] bg-[#E87B37] py-[0.3rem] px-[0.3rem] sm:px-[1rem] md:px-[1rem] md:text-[0.85rem] md:py-[0.5rem] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
               CONTACT US
             </button>
-          </div>
+          </motion.div>
 
           <div className="md:hidden">
             <i
@@ -106,27 +106,62 @@ const Home = () => {
         </nav>
 
         <main className="mt-[10rem]">
-          <div className="border-b-2 border-[#E87B37] w-[6rem] sm:w-[7rem] md:w-[10rem] lg:w-[12rem]"></div>
+          <motion.div
+            className="border-b-2 border-[#E87B37] w-[6rem] sm:w-[7rem] md:w-[10rem] lg:w-[12rem]"
+            initial={{ opacity: 0}}
+            animate={{ opacity: 1 }}
+            transition={{ ease: "easeInOut", duration: 1, delay: 1 }}
+          ></motion.div>
           <div className="text-white">
-            <h1 className=" font-[serif-regular] text-3xl sm:text-4xl md:text-5xl lg:text-6xl pt-[0.5rem]">
+            <motion.h1
+              className="font-[serif-regular] text-3xl sm:text-4xl md:text-5xl lg:text-6xl pt-[0.5rem]"
+              initial={{ opacity: 0, x: -80 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeInOut", duration: 1, delay: 1 }}
+            >
               An Innovative Agency For
-            </h1>
-            <h1 className="font-[serif-regular] text-3xl sm:text-4xl md:text-5xl lg:text-6xl pt-[0.5rem]">
+            </motion.h1>
+            <motion.h1
+              className="font-[serif-regular] text-3xl sm:text-4xl md:text-5xl lg:text-6xl pt-[0.5rem]"
+              initial={{ opacity: 0, x: -80 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeInOut", duration: 1, delay: 1 }}
+            >
               Forward-thinking Brands.
-            </h1>
-            <p className="font-[poppins-regular] pt-[0.5rem] text-[0.6rem] md:text-sm">
+            </motion.h1>
+            <motion.p
+              className="font-[poppins-regular] pt-[0.5rem] text-[0.6rem] md:text-sm"
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeInOut", duration: 1, delay: 1 }}
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing{" "}
-            </p>
-            <p className="font-[poppins-regular] text-[0.6rem] md:text-sm">
+            </motion.p>
+            <motion.p
+              className="font-[poppins-regular] text-[0.6rem] md:text-sm"
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeInOut", duration: 1, delay: 1 }}
+            >
               elit. Ut elit tellus, luctus nec ullamcorper mattis,
-            </p>
-            <p className="font-[poppins-regular] text-[0.6rem] md:text-sm">
+            </motion.p>
+            <motion.p
+              className="font-[poppins-regular] text-[0.6rem] md:text-sm"
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeInOut", duration: 1, delay: 1 }}
+            >
               pulvinar dapibus leo.
-            </p>
-
-            <button className="px-[1rem] py-[0.5rem] mt-[1rem] text-[#E87B37] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-[poppins-regular] text-[0.7rem] sm:text-[0.6rem] md:text-sm lg:text-lg border-[#E87B37] text-align text-center border-2 w-[7rem] sm:w-[8rem] md:w-[10rem]">
-              Learn More
-            </button>
+            </motion.p>
+            <motion.div
+              initial={{ opacity: 0, x: -120 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ ease: "easeInOut", duration: 1, delay: 1 }}
+            >
+              <button className="px-[1rem] py-[0.5rem] mt-[1rem] text-[#E87B37] transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 font-[poppins-regular] text-[0.7rem] sm:text-[0.6rem] md:text-sm lg:text-lg border-[#E87B37] text-align text-center border-2 w-[7rem] sm:w-[8rem] md:w-[10rem]">
+                Learn More
+              </button>
+            </motion.div>
           </div>
         </main>
       </header>
