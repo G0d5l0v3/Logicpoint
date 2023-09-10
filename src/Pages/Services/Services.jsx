5a7ebe996c6dc4fb7704d307312a0c5702996acb
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-09-10 19:25:45 +0100

    Update Services.jsx

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -102,73 +102,78 @@ const Services = () => {
     }
   }, [inView, animationRight]);
 
-
-
   return (
     <React.Fragment>
       <Element name="services">
-        <div
-          ref={ref}
-          className="flex flex-col bg-[#F5F5F5]  md:flex md:flex-row justify-center px-[10%] min-h-screen w-full"
-        >
-          <motion.div className="flex-1  pt-[5rem]" animate ={ animationUp }>
-            <motion.div animate={animationUp}>
-            <div>
-              <img src={smallRectangle} alt="bar" className="pt-7" />
-            </div>
-            <div>
-              <h1 className="font-[serif-regular] pt-1 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-black">
-                Hire Us
-              </h1>
-            </div>
+        <div className="w-full min-h-screen bg-[#F5F5F5] ">
+          <div
+            ref={ref}
+            className="flex flex-col md:flex md:flex-row justify-center px-[10%]"
+          >
+            <motion.div className="flex-1  pt-[5rem]" animate={animationUp}>
+              <motion.div animate={animationUp}>
+                <div>
+                  <img src={smallRectangle} alt="bar" className="pt-7" />
+                </div>
+                <div>
+                  <h1 className="font-[serif-regular] pt-1 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-black">
+                    Hire Us For
+                  </h1>
+                </div>
+              </motion.div>
+
+              <div className="flex flex-col md:flex md:flex-row pt-[2rem]">
+                <motion.div animate={animationReveal} className="md:pr-[1rem]">
+                  <h1 className="font-[serif-regular] text-sm sm:text-lg md:text-xl lg:text-2xl pt-6 font-bold text-black">
+                    IT Consulting
+                  </h1>
+                  <p className="font-[poppins-regular] pt-3 text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
+                    Our mission is to empower our clients to use the internet
+                  </p>
+                  <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
+                    to its full potential by providing affordable, effective,
+                  </p>
+                  <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
+                    custom design and marketing solutions.
+                  </p>
+                </motion.div>
+
+                <motion.div animate={animationReveal} className="pr-[1rem]">
+                  <h1 className="font-[serif-regular] text-sm sm:text-lg md:text-xl lg:text-2xl  pt-6 font-bold text-black">
+                    Development
+                  </h1>
+                  <p className="font-[poppins-regular] pt-3 text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
+                    Our vision is to become a global leader in providing the
+                  </p>
+                  <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
+                    best and unique web design and marketing services to
+                  </p>
+                  <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
+                    improve our client’s productivity and business strength.
+                  </p>
+                </motion.div>
+
+                <motion.div animate={animationReveal}>
+                  <h1 className="font-[serif-regular] text-sm sm:text-lg md:text-xl lg:text-2xl  pt-6 font-bold text-black">
+                    Installation
+                  </h1>
+                  <p className="font-[poppins-regular] pt-3 text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
+                    Our vision is to become a global leader in providing the
+                  </p>
+                  <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
+                    best and unique web design and marketing services to
+                  </p>
+                  <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
+                    improve our client’s productivity and business strength.
+                  </p>
+                </motion.div>
+              </div>
             </motion.div>
-
-            <div className="flex flex-col md:flex md:flex-row pt-[2rem]">
-              <motion.div animate = {animationReveal} className="md:pr-[1rem]">
-                <h1 className="font-[serif-regular] text-sm sm:text-lg md:text-xl lg:text-2xl pt-6 font-bold text-black">
-                  IT Consulting
-                </h1>
-                <p className="font-[poppins-regular] pt-3 text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
-                  Our mission is to empower our clients to use the internet
-                </p>
-                <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
-                  to its full potential by providing affordable, effective,
-                </p>
-                <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
-                  custom design and marketing solutions.
-                </p>
-              </motion.div>
-
-              <motion.div animate = {animationReveal} className="pr-[1rem]">
-                <h1 className="font-[serif-regular] text-sm sm:text-lg md:text-xl lg:text-2xl  pt-6 font-bold text-black">
-                  Development
-                </h1>
-                <p className="font-[poppins-regular] pt-3 text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
-                  Our vision is to become a global leader in providing the
-                </p>
-                <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
-                  best and unique web design and marketing services to
-                </p>
-                <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
-                  improve our client’s productivity and business strength.
-                </p>
-              </motion.div>
-
-              <motion.div animate={animationReveal} >
-                <h1 className="font-[serif-regular] text-sm sm:text-lg md:text-xl lg:text-2xl  pt-6 font-bold text-black">
-                  Installation
-                </h1>
-                <p className="font-[poppins-regular] pt-3 text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
-                  Our vision is to become a global leader in providing the
-                </p>
-                <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
-                  best and unique web design and marketing services to
-                </p>
-                <p className="font-[poppins-regular] text-[0.6rem] sm:text-[0.7rem] md:text-[1rem] text-black">
-                  improve our client’s productivity and business strength.
-                </p>
-              </motion.div>
-            </div>
+          </div>
+          <motion.div className="mt-[5rem]" animate = {animationRight}>
+            <h1 className="font-[serif-regular] text-center pt-1 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-black">
+              Clients We Work With
+            </h1>
           </motion.div>
         </div>
       </Element>
