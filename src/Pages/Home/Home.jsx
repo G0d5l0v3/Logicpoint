4726c5accc7d8eb763f993b397a7aa72e49ca5c0
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-11-08 13:19:26 +0100

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/navbar/Navbar";
 
 const Home = () => {
   return (
-    <div className="">
+    <div>
       <Navbar />
       <div className="flex h-screen max-w-[1440px] mx-auto px-[5rem] py-[5rem]">
         <div className="py-[3rem] text-[#181830]">
@@ -15,7 +15,7 @@ const Home = () => {
           </h1>
           <h1 className="font-[exo] font-semibold text-6xl">Tech Solutions</h1>
         </div>
-        <div className="grid "></div>
+        <div className="grid"></div>
       </div>
     </div>
   );
