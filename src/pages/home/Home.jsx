15daf7cf378557aca91823b9352e046e1d5ec1c2
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-11-08 14:37:33 +0100

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
-import image1 from "../../assets/images/image1.jpg"
-import image2 from "../../assets/images/image2.jpg"
-import image3 from "../../assets/images/image3.jpg"
-import image4 from "../../assets/images/image4.jpg"
+import image1 from "../../assets/images/image1.jpg";
+import image2 from "../../assets/images/image2.jpg";
+import image3 from "../../assets/images/image3.jpg";
+import image4 from "../../assets/images/image4.jpg";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex justify-between h-[80svh] items-center max-w-[1440px] mx-auto px-[5rem] ">
+      <div className="flex justify-between h-[70svh] items-center max-w-[1440px] mx-auto px-[5rem] ">
         <div className="text-[#181830]">
           <h1 className="font-[exo] font-semibold text-6xl">
             Empowering Brands
@@ -20,8 +20,8 @@ const Home = () => {
           <h1 className="font-[exo] font-semibold text-6xl">Tech Solutions</h1>
         </div>
         <div className="grid">
-          <div></div>
-          <div></div>
+          <div className="grid"></div>
+          <div className="grid"></div>
         </div>
       </div>
     </div>
