ff8d8eca67780bc2ab6c5bbb2d08fad122d38613
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-11-08 14:29:30 +0100

    update Home.js

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
+import image1 from "../../assets/images/image1.jpg"
+import image2 from "../../assets/images/image2.jpg"
+import image3 from "../../assets/images/image3.jpg"
+import image4 from "../../assets/images/image4.jpg"
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex justify-between items-center h-screen max-w-[1440px] mx-auto px-[5rem] py-[5rem]">
+      <div className="flex justify-between h-[80svh] items-center max-w-[1440px] mx-auto px-[5rem] ">
         <div className="text-[#181830]">
           <h1 className="font-[exo] font-semibold text-6xl">
             Empowering Brands
