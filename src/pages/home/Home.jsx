596d68514e4b704562486829eba76bada97653e3
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-11-08 14:42:57 +0100

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
-import image1 from "../../assets/images/image1.jpg";
-import image2 from "../../assets/images/image2.jpg";
-import image3 from "../../assets/images/image3.jpg";
-import image4 from "../../assets/images/image4.jpg";
+//import image1 from "../../assets/images/image1.jpg";
+//import image2 from "../../assets/images/image2.jpg";
+//import image3 from "../../assets/images/image3.jpg";
+//import image4 from "../../assets/images/image4.jpg";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <div className="flex justify-between h-[70svh] items-center max-w-[1440px] mx-auto px-[5rem] ">
-        <div className="text-[#181830]">
+        <div className="py-[2rem] text-[#181830]">
           <h1 className="font-[exo] font-semibold text-6xl">
             Empowering Brands
           </h1>
