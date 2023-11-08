f7b2293680b63341e8d188d05f637c71ab5e9396
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-11-08 16:05:34 +0100

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,16 +11,16 @@ const Home = () => {
       <Navbar />
       <div className="flex h-[80svh] justify-between items-center max-w-[1440px] mx-auto px-[5rem] ">
         <div className="flex flex-col h-[40svh]text-[#181830]">
-          <h1 className="font-[exo] font-semibold text-6xl py-[0.3rem]">
+          <h1 className="font-[exo] font-semibold text-3xl sm:text-5xl lg:text-6xl py-[0.3rem]">
             Empowering Brands
           </h1>
-          <h1 className="font-[exo] font-semibold text-6xl py-[0.3rem]">
+          <h1 className="font-[exo] font-semibold text-3xl sm:text-5xl lg:text-6xl py-[0.3rem]">
             With <span className="text-[#E87B37] ">Seamless</span>
           </h1>
-          <h1 className="font-[exo] font-semibold text-6xl py-[0.3rem]">IT Solutions</h1>
-          <p className="font-[poppins] pt-[2rem] pb-[0.3rem}">We deploy the best-in-class technology to meet the unique  </p>
-          <p className="font-[poppins] pb-[0.15rem]">needs of each of our clients. Our solutions are at the</p>
-          <p className="font-[poppins]">cutting edge of contact centre business.</p>
+          <h1 className="font-[exo] font-semibold text-3xl sm:text-5xl lg:text-6xl py-[0.3rem]">IT Solutions</h1>
+          <p className="font-[poppins] text-sm sm:text-md lg:text-lg pt-[2rem] pb-[0.3rem}">We deploy the best-in-class technology to meet the unique  </p>
+          <p className="font-[poppins] text-sm sm:text-md lg:text-lg pb-[0.15rem]">needs of each of our clients. Our solutions are at the</p>
+          <p className="font-[poppins] text-sm sm:text-md lg:text-lg">cutting edge of contact centre business.</p>
         </div>
         <div className="grid">
           <div className="grid"></div>
