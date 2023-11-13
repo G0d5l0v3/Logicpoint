5e5cf3f233f7ca20ea2a70aa89f1739cc823d7c7
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-11-13 08:11:13 +0100

    update

--- src/pages/home/OurServices.jsx
+++ src/pages/home/OurServices.jsx
@@ -12,7 +12,7 @@ const OurServices = () => {
   return (
     <div className="">
       <div
-        className="md:flex md:flex-col md:pt-3 min-h-screen md:min-h-[70svh] grid items-center justify-between max-w-[1440px] mx-auto px-[2rem] sm:px-[5rem]"
+        className="md:flex md:flex-col pt-3 md:pt-3 min-h-screen md:min-h-[70svh] grid items-center justify-between max-w-[1440px] mx-auto px-[2rem] sm:px-[5rem]"
         data-aos="fade-up"
         data-aos-duration="1000"
       >
