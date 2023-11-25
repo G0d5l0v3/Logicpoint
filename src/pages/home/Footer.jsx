b4841de0eff11834bd30f6263e7f906a6000adbb
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-11-25 13:54:34 +0100

    Update Footer.jsx

--- src/pages/home/Footer.jsx
+++ src/pages/home/Footer.jsx
@@ -4,45 +4,41 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="bg-[#313242] font-[exo] text-white">
-      <div className="grid grid-cols-2 place-content-center gap-3 min-h-screen max-w-[1440px] mx-auto px-[2rem] sm:px-[5rem]">
-        <div className="rounded-lg bg-[white] shadow-lg h-[30svh] p-[5%]">
-          <img src={logo} alt="company-logo" className="w-25 h-10" />
+    <div className="bg-[#0a0a1a] font-[exo] text-white">
+      <div className="grid md:grid-cols-2 place-content-center gap-3 min-h-screen max-w-[1440px] mx-auto px-[2rem] sm:px-[5rem]">
+        <div className="">
+
         </div>
-        <div className="flex justify-around ">
+        <div className="flex justify-around">
           <div className="flex flex-col gap-3">
-            <h1 className="font-bold text-[#E87B37] text-xl">Quick Link</h1>
-            <NavLink to="/about-us" className="text-sm sm:text-md lg:text-lg">
+            <h1 className="font-bold text-[#E87B37] text-md sm:text-lg lg:text-xl">Quick Link</h1>
+            <NavLink to="/about-us" className="text-sm sm:text-md lg:text-lg hover:opacity-[0.7]">
               About Us
             </NavLink>
-            <NavLink to="/services" className="text-sm sm:text-md lg:text-lg">
+            <NavLink to="/services" className="text-sm sm:text-md lg:text-lg hover:opacity-[0.7]">
               Services
             </NavLink>
-            <NavLink to="/blog" className="text-sm sm:text-md lg:text-lg">
+            <NavLink to="/blog" className="text-sm sm:text-md lg:text-lg hover:opacity-[0.7]">
               Blog
             </NavLink>
-            <NavLink to="/leadership" className="text-sm sm:text-md lg:text-lg">
+            <NavLink to="/leadership" className="text-sm sm:text-md lg:text-lg hover:opacity-[0.7]">
               Leadership
             </NavLink>
           </div>
           <div className="flex flex-col gap-3">
-            <h1 className="font-bold text-[#E87B37] text-xl">Resources</h1>
-            <NavLink to="/about-us" className="text-sm sm:text-md lg:text-lg">
+            <h1 className="font-bold text-[#E87B37] text-md sm:text-lg lg:text-xl">Resources</h1>
+            <NavLink to="" className="text-sm sm:text-md lg:text-lg hover:opacity-[0.7]">
               Gallery
             </NavLink>
-            <NavLink to="/leadership" className="text-sm sm:text-md lg:text-lg">
+            <NavLink to="" className="text-sm sm:text-md lg:text-lg hover:opacity-[0.7]">
               Case Studies
             </NavLink>
-            <NavLink to="/services" className="text-sm sm:text-md lg:text-lg">
+            <NavLink to="" className="text-sm sm:text-md lg:text-lg hover:opacity-[0.7]">
               Privacy Policy
             </NavLink>
-            <NavLink to="/blog" className="text-sm sm:text-md lg:text-lg">
-              Articles
-            </NavLink>
-
           </div>
           <div className="flex flex-col gap-3">
-            <h1 className="font-bold text-[#E87B37] text-xl">Quick Contact</h1>
+            <h1 className="font-bold text-[#E87B37] text-md sm:text-lg lg:text-xl">Quick Contact</h1>
             <p className="text-sm sm:text-md lg:text-lg">
               support@logicpointng.com
             </p>
@@ -50,6 +46,9 @@ const Footer = () => {
             <p className="text-sm sm:text-md lg:text-lg">US: +1 (609) 874-9097</p>
           </div>
         </div>
+      </div>
+      <div>
+        <p></p>
       </div>
     </div>
   );
