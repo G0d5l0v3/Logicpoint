00c4558cb07602cf9c2fd8172bb20dde65fcc05e
Godslove Udo <udogodslove@gmail.com>
2024-02-17 04:19:02 +0100

    update Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,8 @@
 import Logo from "../logo/Logo";
 import { NavLink } from "react-router-dom";
+import mail from "../../assets/images/mail.png";
+import phone from "../../assets/images/phone-call.png";
+import location from "../../assets/images/location.png";
 
 const Footer = () => {
   return (
@@ -60,15 +63,24 @@ const Footer = () => {
             <h1 className="font-bold text-[#5D1570] text-md sm:text-lg lg:text-lg">
               Contact
             </h1>
-            <p className="text-sm sm:text-md lg:text-md">
-              support@logicpointng.com
-            </p>
-            <p className="text-sm sm:text-md lg:text-md">
-              22, Idowu Taylor Street, Victoria Island, Lagos
-            </p>
-            <p className="text-sm sm:text-md lg:text-md">
-              NG: +234 (201) 330 2905
-            </p>
+            <div className="flex items-center gap-3">
+              <img src={mail} className="w-5 h-5" alt="" />
+              <p className="text-sm sm:text-md lg:text-md">
+                support@logicpointng.com
+              </p>
+            </div>
+            <div className="flex items-center gap-3">
+              <img src={location} className="w-5 h-5" alt="" />
+              <p className="text-sm sm:text-md lg:text-md">
+                22, Idowu Taylor Street, Victoria Island, Lagos
+              </p>
+            </div>
+            <div className="flex items-center gap-3">
+              <img src={phone} className="w-5 h-5" alt="" />
+              <p className="text-sm sm:text-md lg:text-md">
+                +234 (201) 330 2905
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -76,7 +88,9 @@ const Footer = () => {
         <Logo style="w-25 h-10" />
       </div>
       <div className="flex items-center justify-center h-[5rem] border-t-[0.5px] mt-[2rem] lg:mt-0 border-[#5D1570] max-w-[1240px] mx-auto">
-        <p className="text-[.5rem] sm:text-[.7rem] lg:text-[.9rem]">Copyright © 2024 Logicpoint Integrated Concepts Limited</p>
+        <p className="text-[.5rem] sm:text-[.7rem] lg:text-[.9rem]">
+          Copyright © 2024 Logicpoint Integrated Concepts Limited
+        </p>
       </div>
     </div>
   );
