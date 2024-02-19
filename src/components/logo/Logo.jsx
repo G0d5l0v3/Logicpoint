1e128226253a01743383a51645cfe03d36f6bcf5
Godslove Udo <udogodslove@gmail.com>
2024-02-19 11:06:37 +0100

    update ContactUs.jsx

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,19 +1,8 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/Logicpoint-Logo.svg";
 
 const Logo = ({ style }) => {
-  const icon = {
-    hidden: {
-      pathLength: 0,
-      fill: "rgba(232, 123, 55, 1)",
-    },
-    visible: {
-      pathLength: 1,
-      fill: "rgba(255,255,253,57)",
-    },
-  };
   return (
     <Link to="/">
       <img
