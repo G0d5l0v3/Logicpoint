72021d3cac386a85886f782e29562022176b3c4c
Godslove Udo <udogodslove@gmail.com>
2024-04-02 10:17:04 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useState, useEffect } from "react";
+import React, { lazy, Suspense} from "react";
 import { Routes, Route } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import Navbar from "./components/navbar/Navbar";
